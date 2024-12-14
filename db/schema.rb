c25bd0993ed97c8e517f3fc5efc5a90ea3c301a0
Andrew Gerst <andrew.gerst@checkr.com>
2024-12-14 15:48:03 -0800

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_14_234255) do
+  create_table "properties", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "units", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "property_id", null: false
+    t.integer "bedroom_count"
+    t.integer "bathroom_count"
+    t.integer "square_footage"
+    t.decimal "rent_price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["property_id"], name: "index_units_on_property_id"
+  end
+
+  add_foreign_key "units", "properties"
 end
